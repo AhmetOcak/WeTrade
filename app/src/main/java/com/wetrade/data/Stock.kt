4e4b8b4a3c6fde396fb9df7cb14a5cc8ca9a9972
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-10-01 19:39:33 +0300

    app data ready

---
+++ app/src/main/java/com/wetrade/data/Stock.kt
@@ -0,0 +1,9 @@
+package com.wetrade.data
+
+data class Stock(
+    val valuePrice: String,
+    val valueStatus: String,
+    val valueName: String,
+    val valueCompany: String,
+    val valueGraphId: Int
+)
---
+++ app/src/main/java/com/wetrade/data/WeTradeData.kt
@@ -0,0 +1,117 @@
+package com.wetrade.data
+
+import com.wetrade.R
+
+object WeTradeData {
+
+    val StockData = listOf(
+        Stock(
+            valuePrice = "7.918",
+            valueStatus = "-0.54",
+            valueName = "ALK",
+            valueCompany = "Alaska Air Group, Inc",
+            valueGraphId = R.drawable.ic_home_alk
+        ),
+        Stock(
+            valuePrice = "1,293",
+            valueStatus = "+4.18",
+            valueName = "BA",
+            valueCompany = "Boeing Co.",
+            valueGraphId = R.drawable.ic_home_ba
+        ),
+        Stock(
+            valuePrice = "893.50",
+            valueStatus = "-0.54",
+            valueName = "DAL",
+            valueCompany = "Delta Airlines Inc.",
+            valueGraphId = R.drawable.ic_home_dal
+        ),
+        Stock(
+            valuePrice = "12.301",
+            valueStatus = "+2.51",
+            valueName = "EXPE",
+            valueCompany = "Expedia Group Inc.",
+            valueGraphId = R.drawable.ic_home_exp
+        ),
+        Stock(
+            valuePrice = "12,301",
+            valueStatus = "+1.38",
+            valueName = "EADSY",
+            valueCompany = "Airbus SE",
+            valueGraphId = R.drawable.ic_home_eadsy
+        ),
+        Stock(
+            valuePrice = "8.521",
+            valueStatus = "+1.56",
+            valueName = "JBLU",
+            valueCompany = "JetBlue Airways Corp.",
+            valueGraphId = R.drawable.ic_home_jblu
+        ),
+        Stock(
+            valuePrice = "521",
+            valueStatus = "+2.75",
+            valueName = "MAR",
+            valueCompany = "Marriott International Inc.",
+            valueGraphId = R.drawable.ic_home_mar
+        ),
+        Stock(
+            valuePrice = "5.481",
+            valueStatus = "+0.14",
+            valueName = "CCL",
+            valueCompany = "Carnival Corp",
+            valueGraphId = R.drawable.ic_home_ccl
+        ),
+        Stock(
+            valuePrice = "9.184",
+            valueStatus = "+1.69",
+            valueName = "RCL",
+            valueCompany = "Royal Caribbean Cruises",
+            valueGraphId = R.drawable.ic_home_rcl
+        ),
+        Stock(
+            valuePrice = "654",
+            valueStatus = "+3.23",
+            valueName = "TRVL",
+            valueCompany = "Travelocity Inc.",
+            valueGraphId = R.drawable.ic_home_trvl
+        ),
+
+        Stock(
+            valuePrice = "7.918",
+            valueStatus = "-0.54",
+            valueName = "ALK",
+            valueCompany = "Alaska Air Group, Inc",
+            valueGraphId = R.drawable.ic_home_alk
+        ),
+        Stock(
+            valuePrice = "1,293",
+            valueStatus = "+4.18",
+            valueName = "BA",
+            valueCompany = "Boeing Co.",
+            valueGraphId = R.drawable.ic_home_ba
+        ),
+        Stock(
+            valuePrice = "893.50",
+            valueStatus = "-0.54",
+            valueName = "DAL",
+            valueCompany = "Delta Airlines Inc.",
+            valueGraphId = R.drawable.ic_home_dal
+        ),
+        Stock(
+            valuePrice = "12.301",
+            valueStatus = "+2.51",
+            valueName = "EXPE",
+            valueCompany = "Expedia Group Inc.",
+            valueGraphId = R.drawable.ic_home_exp
+        ),
+    )
+
+    val categoryData = listOf(
+        "Week",
+        "ETFs",
+        "Stocks",
+        "Funds",
+        "Flow",
+        "Last",
+    )
+}
